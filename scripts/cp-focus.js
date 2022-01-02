64fee342e94f6d0cdfcb54e67fb2d3dadb5ed892
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-02 21:51:56 +0100

    Update cp-focus to target new servers

--- scripts/cp-focus.js
+++ scripts/cp-focus.js
@@ -1,26 +1,31 @@
 import { executeFocus } from "/scripts/utils.js";
 
 const ATTACK_MAP = {
-    "serv-5": "summit-uni",
-    "serv-6": "crush-fitness",
-    "serv-7": "comptek",
-    "serv-8": "rothman-uni",
-    "serv-9": "millenium-fitness",
-    "serv-10": "netlink",
-    "serv-11": "catalyst",
-    "serv-12": "aevum-police",
-    "serv-13": "snap-fitness",
-    "serv-14": "syscore",
-    "serv-15": "rho-construction",
-    "serv-16": "alpha-ent",
-    "serv-17": "helios",
-    "serv-18": "lexo-corp",
-    "serv-19": "titan-labs",
-    "serv-20": "solaris",
-    "serv-21": "applied-energetics",
-    "serv-22": "zb-institute",
-    "serv-23": "zb-def",
-    "serv-24": "unitalife",
+    "serv-0": "rho-construction",
+    "serv-1": "microdyne",
+    "serv-2": "alpha-ent",
+    "serv-3": "helios",
+    "serv-4": "vitalife",
+    "serv-5": "lexo-corp",
+    "serv-6": "titan-labs",
+    "serv-7": "galactic-cyber",
+    "serv-8": "defcomm",
+    "serv-9": "taiyang-digital",
+    "serv-10": "solaris",
+    "serv-11": "applied-energetics",
+    "serv-12": "omnia",
+    "serv-13": "icarus",
+    "serv-14": "zb-institute",
+    "serv-15": "aerocorp",
+    "serv-16": "zb-def",
+    "serv-17": "unitalife",
+    "serv-18": "nova-med",
+    "serv-19": "univ-energy",
+    "serv-20": "zeus-med",
+    "serv-21": "deltaone",
+    "serv-22": "global-pharm",
+    "serv-23": "omnitek",
+    "serv-24": "blade",
 };
 
 /** @param {NS} ns **/
@@ -29,10 +34,12 @@ export async function main(ns) {
     for (const server of Object.keys(ATTACK_MAP)) {
         const target = ATTACK_MAP[server];
         ns.tprint(`Copying focus script to ${server} to attack ${target}`);
-        await ns.scp(["/scripts/focus-attack.js", "/scripts/utils.js"], server);
-
-        executeFocus(ns, server, target);
-
+        if (!ns.serverExists(server)) {
+            ns.tprint(`${server} doesn't exist, skipping...`);
+        } else {
+            await ns.scp(["/scripts/focus-attack.js", "/scripts/utils.js"], server);
+            executeFocus(ns, server, target);
+        }
         await ns.sleep(250);
     }
 }
