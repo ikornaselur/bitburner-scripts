a99dfb51119fdcd1041983e9578512735a01c4b4
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-03 16:11:36 +0100

    Add a terminate-all script

---
+++ scripts/terminate-all.js
@@ -0,0 +1,27 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    const servers = {
+        home: false,
+    };
+
+    let unscanned;
+    while (
+        (unscanned = Object.keys(servers).filter((server) => !servers[server]))
+            .length > 0
+    ) {
+        for (const server of unscanned) {
+            ns.tprint(`Scanning ${server}`);
+            if (server !== "home") {
+                ns.killall(server);
+            }
+            const connectedServers = ns.scan(server);
+            servers[server] = true;
+            for (const connected of connectedServers) {
+                if (!servers[connected]) {
+                    servers[connected] = false;
+                }
+            }
+        }
+        await ns.sleep(100);
+    }
+}
