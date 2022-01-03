ce2f973d89c5191ecab87805a168964e9bc5227f
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-03 19:54:46 +0100

    Update init-hack to be more dynamic, properly this time

--- scripts/init-hack.js
+++ scripts/init-hack.js
@@ -2,91 +2,37 @@ import { SERVERS_MAP } from "/scripts/constants.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    const hackTools = [
-        "BruteSSH",
-        "FTPCrack",
-        "HTTPWorm",
-        "SQLInject",
-        "relaySMTP",
-    ];
-    const toolCount = hackTools.filter((tool) =>
-        ns.fileExists(`${tool}.exe`)
-    ).length;
+  const hackTools = [
+    "BruteSSH",
+    "FTPCrack",
+    "HTTPWorm",
+    "SQLInject",
+    "relaySMTP",
+  ];
+  const ownedTools = hackTools.filter((tool) => ns.fileExists(`${tool}.exe`));
 
-    for (const server of SERVERS_MAP[0]) {
-        if (!ns.hasRootAccess(server)) {
-            ns.tprint(`Getting root access on ${server}`);
-            ns.nuke(server);
+  for (let serverIdx = 0; serverIdx < ownedTools.length + 1; serverIdx++) {
+    ns.tprint(`serverIdx: ${serverIdx}`);
+    for (const server of SERVERS_MAP[serverIdx]) {
+      if (!ns.hasRootAccess(server)) {
+        ns.tprint(`[${server}] Getting root access`);
+        if (ownedTools.indexOf("BruteSSH") > -1) {
+          ns.brutessh(server);
         }
-    }
-
-    if (toolCount < 1) {
-        return;
-    }
-
-    for (const server of SERVERS_MAP[1]) {
-        if (!ns.hasRootAccess(server)) {
-            ns.tprint(`Getting root access on ${server}`);
-            ns.brutessh(server);
-            ns.nuke(server);
+        if (ownedTools.indexOf("FTPCrack") > -1) {
+          ns.ftpcrack(server);
         }
-    }
-
-    if (toolCount < 2) {
-        return;
-    }
-
-    for (const server of SERVERS_MAP[2]) {
-        if (!ns.hasRootAccess(server)) {
-            ns.tprint(`Getting root access on ${server}`);
-            ns.ftpcrack(server);
-            ns.brutessh(server);
-            ns.nuke(server);
+        if (ownedTools.indexOf("HTTPWorm") > -1) {
+          ns.httpworm(server);
         }
-    }
-
-    if (toolCount < 3) {
-        return;
-    }
-
-    for (const server of SERVERS_MAP[3]) {
-        if (!ns.hasRootAccess(server)) {
-            ns.tprint(`Getting root access on ${server}`);
-            ns.httpworm(server);
-            ns.ftpcrack(server);
-            ns.brutessh(server);
-            ns.nuke(server);
+        if (ownedTools.indexOf("SQLInject") > -1) {
+          ns.sqlinject(server);
         }
-    }
-
-    if (toolCount < 4) {
-        return;
-    }
-
-    for (const server of SERVERS_MAP[4]) {
-        if (!ns.hasRootAccess(server)) {
-            ns.tprint(`Getting root access on ${server}`);
-            ns.sqlinject(server);
-            ns.httpworm(server);
-            ns.ftpcrack(server);
-            ns.brutessh(server);
-            ns.nuke(server);
+        if (ownedTools.indexOf("relaySMTP") > -1) {
+          ns.relaysmtp(server);
         }
+        ns.nuke(server);
+      }
     }
-
-    if (toolCount < 5) {
-        return;
-    }
-
-    for (const server of SERVERS_MAP[5]) {
-        if (!ns.hasRootAccess(server)) {
-            ns.tprint(`Getting root access on ${server}`);
-            ns.relaysmtp(server);
-            ns.sqlinject(server);
-            ns.httpworm(server);
-            ns.ftpcrack(server);
-            ns.brutessh(server);
-            ns.nuke(server);
-        }
-    }
+  }
 }
