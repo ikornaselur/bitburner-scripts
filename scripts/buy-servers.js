9617157e7e7288e933b395b37f42a7e3d1ecf018
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-03 17:24:13 +0100

    Update buy-servers to handle invalid RAM inputs

--- scripts/buy-servers.js
+++ scripts/buy-servers.js
@@ -1,9 +1,21 @@
 import { PREFIX } from "/scripts/constants.js";
-import { humanReadableMoney } from "/scripts/utils.js";
+import { humanReadableMoney, humanReadableRAM } from "/scripts/utils.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
     const RAM = parseInt(ns.args[0]) * 1024; // In GB
+
+    // Store cost of the server
+    const serverCost = ns.getPurchasedServerCost(RAM);
+    if (serverCost === Infinity) {
+        ns.tprint("Invalid RAM value");
+        ns.exit();
+    }
+    ns.tprint(
+        `${humanReadableRAM(RAM)} server cost: ${humanReadableMoney(
+            serverCost
+        )}`
+    );
 
     // Base server idx
     let i = 0;
@@ -12,9 +24,6 @@ export async function main(ns) {
         ns.tprint(`${PREFIX}${i} already exists, skipping...`);
         i++;
     }
-    // Store cost of the server
-    const serverCost = ns.getPurchasedServerCost(RAM);
-    ns.tprint(`${RAM}GB server cost: ${humanReadableMoney(serverCost)}`);
 
     while (i < ns.getPurchasedServerLimit()) {
         if (ns.getServerMoneyAvailable("home") < serverCost) {
