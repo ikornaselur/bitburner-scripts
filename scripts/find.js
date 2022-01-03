5f157da0915f104709015aa45527429fbccec435
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-04 00:53:15 +0100

    Update find.js to list connection strings to all backdoorable nodes

--- scripts/find.js
+++ scripts/find.js
@@ -1,26 +1,32 @@
+const BACKDOOR_TARGETS = ["CSEC", "I.I.I.I", "avmnite-02h", "run4theh111z"];
+
 /** @param {NS} ns
  * @param {string} current
  * @param {string} target
  * @param {Array<string>} path
  * @param {Array<string>} visited
  */
-async function find(ns, current, target, path, visited) {
+async function find(ns, current, target, path, visited, backdoor) {
   if (current === target) {
-    ns.tprint(
-      path
+    let output = path
         .concat(current)
         .slice(1)
         .map((server) => `ssh ${server}`)
         .join("; ")
-    );
-    ns.exit();
+    if (backdoor) {
+      output += "; backdoor";
+    }
+    ns.tprint(output);
+    return true;
   } else {
     const connectedServers = ns
       .scan(current)
       .filter((s) => visited.indexOf(s) === -1);
     for (const connected of connectedServers) {
       visited.push(connected);
-      await find(ns, connected, target, path.concat(current), visited);
+      if (await find(ns, connected, target, path.concat(current), visited, backdoor)) {
+        return true;
+      }
       await ns.sleep(10);
     }
   }
@@ -29,9 +35,15 @@ async function find(ns, current, target, path, visited) {
 /** @param {NS} ns **/
 export async function main(ns) {
   const target = ns.args[0];
-  if (!ns.serverExists(target)) {
-    ns.tprint(`"${target}" doesn't exist. Aborting...`);
-    ns.exit();
+  if (target === "backdoor") {
+    for (const target of BACKDOOR_TARGETS) {
+      await find(ns, "home", target, [], ["home"], true);
+    }
+  } else {
+    if (!ns.serverExists(target)) {
+      ns.tprint(`"${target}" doesn't exist. Aborting...`);
+      ns.exit();
+    }
+    await find(ns, "home", target, [], ["home"], false);
   }
-  await find(ns, "home", target, [], ["home"]);
 }
