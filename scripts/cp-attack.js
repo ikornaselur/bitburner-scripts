878c245cfaebe8ab06d5675f608aafb63af4ec41
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-07 20:11:49 +0000

    Update cp-attack to only target company servers

--- scripts/cp-attack.js
+++ scripts/cp-attack.js
@@ -6,7 +6,6 @@ const CP_SLEEP = 50;
 /** @param {NS} ns **/
 export async function main(ns) {
   const target = ns.args[0];
-  const home = ns.args[1] === "--home";
 
   ns.tprint("Copying attack script to servers");
   for (const server of SERVERS) {
@@ -23,32 +22,5 @@ export async function main(ns) {
     executeAttack(ns, server, target, availMem);
     ns.tprint("-------------------");
     await ns.sleep(CP_SLEEP);
-  }
-
-  // Copy to the serv-X servers
-  let i = 0;
-  let server = `serv-${i}`;
-  while (ns.serverExists(server)) {
-    ns.tprint(`[${server}] Copying script`);
-    await scpAttackScripts(ns, server);
-
-    const availMem = ns.getServerMaxRam(server);
-    executeAttack(ns, server, target, availMem);
-    ns.tprint("-------------------");
-
-    i++;
-    server = `serv-${i}`;
-    await ns.sleep(CP_SLEEP);
-  }
-
-  if (home) {
-    // Execute attack on home as well
-    const homeMem = ns.getServerMaxRam("home");
-    for (const process of ns.ps("home")) {
-      if (process.filename.indexOf("/scripts/attack.js") > -1) {
-        ns.kill(process.pid, "home");
-      }
-    }
-    executeAttack(ns, "home", target, homeMem, false);
   }
 }
