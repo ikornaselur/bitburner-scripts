8e38a608df51fb169a90dc798d5762995c237908
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-08 17:43:40 +0000

    Update hyper/dist to cap hack ram used for low spec nodes

--- scripts/hyper/dist.js
+++ scripts/hyper/dist.js
@@ -8,6 +8,8 @@ function getAvailableRam(ns) {
   return maxRam - usedRam - 5; // Leave ~5GB additional for ad-hoc scripts
 }
 
+const MONEY_THRESH = 0.75;
+
 /** @param {NS} ns **/
 export async function main(ns) {
   const target = ns.args[0];
@@ -19,23 +21,29 @@ export async function main(ns) {
     // Times in milliseconds
     const hackTime = ns.getHackTime(target);
 
-    const hackThreads = ns.hackAnalyzeThreads(target, maxMoney / 2);
-    ns.print("hackThreads: ", hackThreads);
-
     const availableRam = getAvailableRam(ns);
     ns.print("availableRam: ", availableRam);
+
+    const maxThreads = Math.floor(availableRam / ns.getScriptRam(HACK));
+
+    const hackThreads = Math.min(ns.hackAnalyzeThreads(target, maxMoney / 2), maxThreads);
+    ns.print("hackThreads: ", hackThreads);
 
     const hackRam = hackThreads * ns.getScriptRam(HACK);
     ns.print("hackRam: ", hackRam);
 
-    if (ns.getServerMoneyAvailable(target) < maxMoney) {
+    if (ns.getServerMoneyAvailable(target) < maxMoney * MONEY_THRESH) {
+      ns.print("WARN Not enough money available");
       await ns.sleep(1000);
       continue;
     }
-    if (
-      hackRam > availableRam ||
-      ns.getServerSecurityLevel(target) > minSecurity * 3
-    ) {
+    if (hackRam > availableRam) {
+      ns.print("ERROR Not enough ram available to hack!");
+      await ns.sleep(1000);
+      continue;
+    }
+    if (ns.getServerSecurityLevel(target) > minSecurity * 3) {
+      ns.print("WARN Server security level too high");
       await ns.sleep(1000);
       continue;
     }
