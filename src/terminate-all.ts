ae6ed22f75bcc3ffca4c48b8461a31e2dfef0e22
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-11 22:59:24 +0000

    Port over terminate-all script

---
+++ src/terminate-all.ts
@@ -0,0 +1,37 @@
+import { NS } from 'bitburner';
+import { NODES } from "/scripts/constants.js";
+
+export const main = async (ns: NS): Promise<void> => {
+  let servOnly = false;
+  if (ns.args[0] === "serv") {
+    servOnly = true;
+  }
+  const servers = {
+    home: false,
+  };
+
+  let unscanned: Array<string>;
+  while (
+    (unscanned = Object.keys(servers).filter((server) => !servers[server]))
+      .length > 0
+  ) {
+    for (const server of unscanned) {
+      ns.tprint(`Scanning ${server}`);
+      if (servOnly && NODES.indexOf(server) > -1) {
+        ns.killall(server);
+      } else {
+        if (server !== "home") {
+          ns.killall(server);
+        }
+      }
+      const connectedServers = ns.scan(server);
+      servers[server] = true;
+      for (const connected of connectedServers) {
+        if (!servers[connected]) {
+          servers[connected] = false;
+        }
+      }
+    }
+    await ns.sleep(100);
+  }
+}
