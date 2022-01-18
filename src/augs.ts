bb5a20d0779850dfca5a579c676fa76341313a58
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-18 23:21:47 +0000

    Update augs script with more functionality

--- src/augs.ts
+++ src/augs.ts
@@ -1,27 +1,31 @@
 import { NS } from "bitburner";
 import { humanReadableMoney, humanReadable } from "/scripts/utils/format";
+import { FACTIONS } from "/scripts/constants";
 
 const MULT = 1.9;
 
-export const main = async (ns: NS): Promise<void> => {
-  const flags: { faction: string; buy: boolean; help: boolean } = ns.flags([
-    ["faction", "CyberSec"],
-    ["buy", false],
-    ["help", false],
-  ]);
-
-  if (flags.help) {
-    ns.tprint("Usage:");
-    ns.tprint('./augs.js [--faction "Faction Name"] [--buy]');
-    return;
+const findAug = (ns: NS, aug: string): void => {
+  ns.tprint(`Factions with ${aug}:`);
+  for (const faction of FACTIONS) {
+    const augs = ns.getAugmentationsFromFaction(faction);
+    if (augs.indexOf(aug) > -1) {
+      ns.tprint(`  - ${faction}`);
+    }
   }
+};
 
-  const augs = ns.getAugmentationsFromFaction(flags.faction);
+const getFactionAugs = (
+  ns: NS,
+  faction: string,
+  buy = false,
+  info = false
+): void => {
+  const augs = ns.getAugmentationsFromFaction(faction);
   const owned = ns.getOwnedAugmentations(true);
-  const rep = ns.getFactionRep(flags.faction);
+  const rep = ns.getFactionRep(faction);
   const userMoney = ns.getServerMoneyAvailable("home");
 
-  ns.tprint(`INFO Current rep with ${flags.faction}: ${humanReadable(rep)}`);
+  ns.tprint(`INFO Current rep with ${faction}: ${humanReadable(rep)}`);
 
   const augStats = augs
     .filter((aug) => owned.indexOf(aug) === -1)
@@ -46,7 +50,7 @@ export const main = async (ns: NS): Promise<void> => {
     if (aug.rep > rep) {
       logLevel = "ERROR";
     }
-    const buy = flags.buy && logLevel === "INFO";
+    const shouldBuy = buy && logLevel === "INFO";
 
     totalMoneyRequired += cost;
     ns.tprint(
@@ -54,9 +58,16 @@ export const main = async (ns: NS): Promise<void> => {
         cost
       )} ${aug.name}: (${humanReadable(aug.rep)}) `
     );
+    if (info) {
+      const stat = ns.getAugmentationStats(aug.name);
+      for (const key of Object.keys(stat)) {
+        ns.tprint(`  ${key}: ${stat[key]}`);
+      }
+    }
+
     buyCount += 1;
-    if (buy) {
-      if (ns.purchaseAugmentation(flags.faction, aug.name)) {
+    if (shouldBuy) {
+      if (ns.purchaseAugmentation(faction, aug.name)) {
         ns.tprint("SUCCESS Augmentation purchased");
       } else {
         ns.tprint("ERROR FAiled to buy augmentation");
@@ -65,4 +76,31 @@ export const main = async (ns: NS): Promise<void> => {
   }
 
   ns.tprint(`INFO required money: ${humanReadableMoney(totalMoneyRequired)}`);
+};
+
+export const main = async (ns: NS): Promise<void> => {
+  const flags: {
+    faction: string;
+    buy: boolean;
+    help: boolean;
+    info: boolean;
+    find: string;
+  } = ns.flags([
+    ["faction", ""],
+    ["find", ""],
+    ["buy", false],
+    ["info", false],
+    ["help", false],
+  ]);
+
+  if (flags.help) {
+    ns.tprint("Usage:");
+    ns.tprint('./augs.js [--faction "Faction Name"] [--buy] [--info]');
+    ns.tprint('./augs.js [--find "Augmentation name"]');
+    return;
+  } else if (flags.faction.length > 0) {
+    getFactionAugs(ns, flags.faction, flags.buy, flags.info);
+  } else if (flags.find.length > 0) {
+    findAug(ns, flags.find);
+  }
 };
