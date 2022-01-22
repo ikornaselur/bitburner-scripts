59840057b71eb5c3c5f56b54bb8f2e693e247f88
Axel Örn Sigurðsson <dev@absalon.is>
2022-01-22 00:25:29 +0000

    Refactor attack/early/cp.ts to use ns.flags

--- src/attack/early/cp.ts
+++ src/attack/early/cp.ts
@@ -2,7 +2,7 @@ import { NS } from "bitburner";
 import { SERVERS, NODES } from "/scripts/constants.js";
 
 const ATTACK_SRC = "/scripts/attack/early/attack.js";
-const CONSTANTS_SRC = "/scripts/constants.js"
+const CONSTANTS_SRC = "/scripts/constants.js";
 const STATUS_SRC = "/scripts/utils/status.js";
 
 const scpAttackScripts = async (ns: NS, server: string): Promise<void> => {
@@ -31,7 +31,12 @@ const executeAttack = (
 };
 
 export const main = async (ns: NS): Promise<void> => {
-  if (typeof ns.args[0] !== "string") {
+  const flags = ns.flags([
+    ["verbose", false],
+    ["nodes", false],
+  ]);
+  const target = flags["_"][0];
+  if (typeof target !== "string") {
     ns.tprint("Usage: cp.js <target>");
     ns.tprint(
       "Example: cp.js n00dles -> Copies attack scripts to nodes and attacks n00dles"
@@ -39,13 +44,13 @@ export const main = async (ns: NS): Promise<void> => {
     return ns.exit();
   }
 
-  const target = ns.args[0];
-
   ns.tprint("Copying attack script to servers");
   for (const server of SERVERS) {
     if (!ns.hasRootAccess(server)) {
-      // Skip the servers without root access
-      ns.tprint(`ERROR [${server}] No root access!`);
+      if (flags.verbose) {
+        // Skip the servers without root access
+        ns.tprint(`ERROR [${server}] No root access!`);
+      }
       continue;
     }
     ns.tprint(`INFO [${server}] Copying script`);
@@ -56,10 +61,12 @@ export const main = async (ns: NS): Promise<void> => {
     await ns.sleep(10);
   }
 
-  if (ns.args[1] === "--nodes") {
+  if (flags.nodes) {
     for (const node of NODES) {
       if (!ns.serverExists(node)) {
-        ns.tprint(`ERROR [${node}] Node is offline`);
+        if (flags.verbose) {
+          ns.tprint(`ERROR [${node}] Node is offline`);
+        }
         continue;
       }
       ns.tprint(`INFO [${node}] Copying script`);
